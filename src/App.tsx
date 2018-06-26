66fd5473990614aee37d2692054804083816b179
tbingooo <tangbing@dtcj.com>
2018-06-26 11:35:46 +0800

    chore: add network request inspect

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,21 @@ import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import Router from './router';
 
+declare const global: any;
+if (__DEV__) {
+  global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest;
+  global.FormData = global.originalFormData || global.FormData;
+  global.Blob = global.originalBlob || global.Blob;
+  global.FileReader = global.originalFileReader || global.FileReader;
+  
+  fetch; // Ensure to get the lazy property
+
+  // RNDebugger only
+  if ((window as any).__FETCH_SUPPORT__) {
+    (window as any).__FETCH_SUPPORT__.blob = false
+  }
+}
+
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(sagas);

a946858c1fc8ea88e2a7439557442d45e47c355f
tbingooo <tangbing@dtcj.com>
2018-06-27 18:35:42 +0800

    feat: image preview

--- src/containers/detail/index.tsx
+++ src/containers/detail/index.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
-import { connect } from 'react-redux'; 
+import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import Markdown from 'react-native-markdown-renderer';
+import FitImage from 'react-native-fit-image';
+import ImageViewer from 'react-native-image-zoom-viewer';
 import {
   View,
   Text,
   ScrollView,
+  TouchableWithoutFeedback,
+  Modal,
 } from 'react-native';
 import { State, Gist, Action, ActionType, ReactNavigationProps } from 'src/typings';
 import detailSelector from 'src/selectors/detail';
-import cls from './styles';
+import cls, { markdown } from './styles';
 import { Loading } from '../../components';
 
 interface GistDetailProps {
@@ -29,8 +33,12 @@ interface DispatchProps {
 class GistDetail extends React.Component<GistDetailProps & StateProps & DispatchProps & ReactNavigationProps, any> {
   static navigationOptions = ({ navigation }: any) => {
     return {
-      title: typeof(navigation.state.params)==='undefined' || typeof(navigation.state.params.title) === 'undefined' ? '笔记详情': navigation.state.params.title,
+      title: typeof (navigation.state.params) === 'undefined' || typeof (navigation.state.params.title) === 'undefined' ? '笔记详情' : navigation.state.params.title,
     }
+  };
+
+  state = {
+    previewImage: '',
   };
 
   componentDidMount() {
@@ -59,26 +67,63 @@ class GistDetail extends React.Component<GistDetailProps & StateProps & Dispatch
     }
   }
 
+  previewImage = (imageUrl: string) => {
+    this.setState({
+      previewImage: imageUrl,
+    });
+  }
+
+  onCancelPreview = () => {
+    this.setState({
+      previewImage: '',
+    });
+  }
+
+  getCustomRules() {
+    return {
+      image: (node: any, children: any, parent: any, styles: any) => {
+        if (node.type === 'image') {
+          return (
+            <TouchableWithoutFeedback onPress={() => { this.previewImage(node.attributes.src) }}>
+              <FitImage indicator={true} key={node.key} style={styles.image} source={{ uri: node.attributes.src }} />
+            </TouchableWithoutFeedback>
+          );
+        }
+        return null;
+      }
+    };
+  }
+
   render() {
     const { detail, fileData, ui } = this.props;
     const { files } = detail;
+    const { previewImage } = this.state;
+    const previewImages = previewImage ? [{
+      url: previewImage,
+      props: {
+        key: 1,
+      },
+    }] : [];
     return (
       <View style={cls.container}>
         <ScrollView>
-        {
-          files.map(fileName => {
-            return fileData[fileName].content ?
-            <View style={cls.article} key={fileName}>
-              {
-                files.length > 1 ? <Text style={cls.articleTitle}>{fileName.split('.')[0]}</Text> : null
-              }
-              <Markdown>{fileData[fileName].content}</Markdown>
-            </View>
-            : null;
-          })
-        }
+          {
+            files.map(fileName => {
+              return fileData[fileName].content ?
+                <View style={cls.article} key={fileName}>
+                  {
+                    files.length > 1 ? <Text style={cls.articleTitle}>{fileName.split('.')[0]}</Text> : null
+                  }
+                  <Markdown rules={this.getCustomRules()} style={markdown}>{fileData[fileName].content}</Markdown>
+                </View>
+                : null;
+            })
+          }
         </ScrollView>
         <Loading loading={ui.loading} />
+        <Modal visible={!!previewImage} transparent={true} onRequestClose={this.onCancelPreview}>
+          <ImageViewer imageUrls={previewImages} />
+        </Modal>
       </View>
     );
   }
--- src/containers/detail/styles.ts
+++ src/containers/detail/styles.ts
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform, } from 'react-native';
 
 export default StyleSheet.create({
   container: {
@@ -8,9 +8,9 @@ export default StyleSheet.create({
     padding: 10,
   },
   articleTitle: {
-    fontSize: 20,
-    lineHeight: 22,
-    color: '#000000',
+    fontSize: 28,
+    lineHeight: 34,
+    // color: '#000000',
     fontWeight: 'bold',
     marginBottom: 5,
     textAlign: 'center',
@@ -21,5 +21,184 @@ export default StyleSheet.create({
     left: 0,
     width: '100%',
     height: '100%',
+  },
+});
+
+export const markdown = StyleSheet.create({
+  root: {},
+  view: {},
+  codeBlock: {
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    backgroundColor: '#f5f5f5',
+    padding: 10,
+    borderRadius: 4,
+  },
+  codeInline: {
+    borderWidth: 1,
+    borderColor: '#CCCCCC',
+    backgroundColor: '#f5f5f5',
+    padding: 10,
+    borderRadius: 4,
+  },
+  del: {
+    backgroundColor: '#000000',
+  },
+  em: {
+    fontStyle: 'italic',
+  },
+  headingContainer: {
+    flexDirection: 'row',
+  },
+  heading: {
+    fontWeight: 'bold',
+  },
+  heading1: {
+    fontSize: 28,
+    lineHeight: 34,
+  },
+  heading2: {
+    fontSize: 22,
+    lineHeight: 28,
+  },
+  heading3: {
+    fontSize: 20,
+    lineHeight: 26,
+  },
+  heading4: {
+    fontSize: 18,
+    lineHeight: 24,
+  },
+  heading5: {
+    fontSize: 16,
+    lineHeight: 22,
+  },
+  heading6: {
+    fontSize: 14,
+    lineHeight: 20,
+  },
+  hr: {
+    backgroundColor: '#000000',
+    height: 1,
+  },
+  blockquote: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    margin: 20,
+    backgroundColor: '#CCCCCC',
+  },
+  inlineCode: {
+    borderRadius: 3,
+    borderWidth: 1,
+    fontFamily: 'Courier',
+    fontWeight: 'bold',
+  },
+  list: {},
+  listItem: {
+    flex: 1,
+    flexWrap: 'wrap',
+    // backgroundColor: 'green',
+  },
+  listUnordered: {},
+
+  listUnorderedItem: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+
+  listUnorderedItemIcon: {
+    marginLeft: 10,
+    marginRight: 10,
+    ...Platform.select({
+      ios: {
+        lineHeight: 36,
+      },
+      android: {
+        lineHeight: 30,
+      },
+    }),
+  },
+  listUnorderedItemText: {
+    fontSize: 20,
+    lineHeight: 20,
+  },
+
+  listOrdered: {},
+  listOrderedItem: {
+    flexDirection: 'row',
+  },
+  listOrderedItemIcon: {
+    marginLeft: 10,
+    marginRight: 10,
+    ...Platform.select({
+      ios: {
+        lineHeight: 36,
+      },
+      android: {
+        lineHeight: 30,
+      },
+    }),
+  },
+  listOrderedItemText: {
+    fontWeight: 'bold',
+    lineHeight: 20,
+  },
+  paragraph: {
+    marginTop: 10,
+    marginBottom: 10,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  hardbreak: {
+    width: '100%',
+    height: 1,
+  },
+  strong: {
+    fontWeight: 'bold',
+  },
+  table: {
+    borderWidth: 1,
+    borderColor: '#000000',
+    borderRadius: 3,
+  },
+  tableHeader: {},
+  tableHeaderCell: {
+    flex: 1,
+    // color: '#000000',
+    padding: 5,
+    // backgroundColor: 'green',
+  },
+  tableRow: {
+    borderBottomWidth: 1,
+    borderColor: '#000000',
+    flexDirection: 'row',
+  },
+  tableRowCell: {
+    flex: 1,
+    padding: 5,
+  },
+  text: {
+    fontSize: 14,
+    lineHeight: 20,
+  },
+  strikethrough: {
+    textDecorationLine: 'line-through',
+  },
+  link: {
+    textDecorationLine: 'underline',
+  },
+  blocklink: {
+    flex: 1,
+    borderColor: '#000000',
+    borderBottomWidth: 1,
+  },
+  u: {
+    borderColor: '#000000',
+    borderBottomWidth: 1,
+  },
+  image: {
+    flex: 1,
   },
 });
